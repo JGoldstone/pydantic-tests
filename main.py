12e800c270f2d7f5528064819dae851d5f639fdf
jgoldstone <joseph.goldstone@mac.com>
2024-10-03 12:58:44 -0400

    Add dump functions; add Poetry lock and workspace.xml file.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from pydantic_models import Employee
+
 
 def employee_from_ctor() -> Employee:
     return Employee(name="Chris DeTuma",
@@ -6,17 +9,19 @@ def employee_from_ctor() -> Employee:
                     date_of_birth="1998-04-02",
                     salary=123_000.00,
                     department="IT",
-                    elected_benefits=True,)
+                    elected_benefits=True, )
+
 
 def employee_from_dict() -> Employee:
     new_employee_dict = {
-    "name": "Chris DeTuma",
-    "email": "cdetuma@example.com",
-    "date_of_birth": "1998-04-02",
-    "salary": 123_000.00,
-    "department": "IT",
-    "elected_benefits": True}
+        "name": "Chris DeTuma",
+        "email": "cdetuma@example.com",
+        "date_of_birth": "1998-04-02",
+        "salary": 123_000.00,
+        "department": "IT",
+        "elected_benefits": True}
     return Employee.model_validate(new_employee_dict)
+
 
 def employee_from_json() -> Employee:
     new_employee_json = """
@@ -30,8 +35,19 @@ def employee_from_json() -> Employee:
 """
     return Employee.model_validate_json(new_employee_json)
 
+
+def dump_employee_as_string(employee: Employee) -> dict[str, Any]:
+    return employee.model_dump()
+
+
+def dump_employee_as_json(employee: Employee) -> str:
+    return employee.model_dump_json()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     employee_from_ctor()
     employee_from_dict()
-    employee_from_json()
+    e: Employee = employee_from_json()
+    e_as_dict: dict[str, Any] = dump_employee_as_string(e)
+    e_as_json: str = dump_employee_as_json(e)
