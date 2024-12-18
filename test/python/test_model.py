4472d34634cec9e7b8a4a44ad2bcb190b116646d
Joseph Goldstone <joseph.goldstone@mac.com>
2024-12-18 15:39:58 -0500

    Change test_model.py expectation: no longer assume list or Fraction assigned to a value will read back as list or Fraction; now expect how the field was defined (tuple or StrictlyPositiveRational).

--- test/python/test_model.py
+++ test/python/test_model.py
@@ -161,10 +161,10 @@ class ModelTest(unittest.TestCase):
     self.assertTupleEqual(d["sourceNumber"], (1, 2))
     self.assertTupleEqual(d["protocol"], ({"name": OPENTRACKIO_PROTOCOL_NAME, "version": OPENTRACKIO_PROTOCOL_VERSION},
                                           {"name": OPENTRACKIO_PROTOCOL_NAME, "version": OPENTRACKIO_PROTOCOL_VERSION}))
-    self.assertTupleEqual(d["relatedSampleIds"], (["urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-                                                   "urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf7"],
-                                                  ["urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf8",
-                                                   "urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf9"]))
+    self.assertTupleEqual(d["relatedSampleIds"], (("urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
+                                                   "urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf7"),
+                                                  ("urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf8",
+                                                   "urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf9")))
     self.assertTupleEqual(d["globalStage"], ({ "E":100.0, "N":200.0, "U":300.0,
                                                "lat0":100.0, "lon0":200.0, "h0":300.0 },
                                              { "E":100.0, "N":200.0, "U":300.0,
@@ -188,7 +188,7 @@ class ModelTest(unittest.TestCase):
                   "offsets": {"translation":1.0,"rotation":2.0,"lensEncoders":3.0 } }
     self.assertTupleEqual(d["timing"]["synchronization"], (sync_dict, sync_dict))
     transform_dict = { "translation": { "x":1.0,"y":2.0,"z":3.0 }, "rotation": { "pan":1.0,"tilt":2.0,"roll":3.0 } }
-    self.assertTupleEqual(d["transforms"], ([transform_dict, transform_dict], [transform_dict, transform_dict]))
+    self.assertTupleEqual(d["transforms"], ((transform_dict, transform_dict), (transform_dict, transform_dict)))
 
     self.assertTupleEqual(d["lens"]["tStop"], (2000, 4000))
     self.assertTupleEqual(d["lens"]["fStop"], (1200, 2800))
@@ -203,10 +203,10 @@ class ModelTest(unittest.TestCase):
     self.assertTupleEqual(d["lens"]["undistortionOverscan"], (1.0, 1.0))
     self.assertTupleEqual(d["lens"]["exposureFalloff"], ({ "a1":1.0,"a2":2.0,"a3":3.0 },
                                                          { "a1":1.0,"a2":2.0,"a3":3.0 }))
-    self.assertTupleEqual(d["lens"]["distortion"], ([{ "radial":[1.0,2.0,3.0], "tangential":[1.0,2.0], "model": "Brown-Conrady D-U"},
-                                                     { "radial":[1.0,2.0,3.0], "tangential":[1.0,2.0], "model": "Brown-Conrady U-D"}],
-                                                    [{ "radial":[1.0,2.0,3.0], "tangential":[1.0,2.0], "model": "Brown-Conrady D-U"},
-                                                     { "radial":[1.0,2.0,3.0], "tangential":[1.0,2.0], "model": "Brown-Conrady U-D"}]))
+    self.assertTupleEqual(d["lens"]["distortion"], (({ "radial":(1.0,2.0,3.0), "tangential":(1.0,2.0), "model": "Brown-Conrady D-U"},
+                                                     { "radial":(1.0,2.0,3.0), "tangential":(1.0,2.0), "model": "Brown-Conrady U-D"}),
+                                                    ({ "radial":(1.0,2.0,3.0), "tangential":(1.0,2.0), "model": "Brown-Conrady D-U"},
+                                                     { "radial":(1.0,2.0,3.0), "tangential":(1.0,2.0), "model": "Brown-Conrady U-D"})))
     self.assertTupleEqual(d["lens"]["distortionOffset"], ({ "x":1.0,"y":2.0 }, { "x":1.0,"y":2.0 }))
     self.assertTupleEqual(d["lens"]["projectionOffset"], ({ "x":0.1,"y":0.2 }, { "x":0.1,"y":0.2 }))
 
@@ -297,7 +297,8 @@ class ModelTest(unittest.TestCase):
 
     clip.capture_frame_rate = value
 
-    self.assertEqual(clip.capture_frame_rate, value)
+    self.assertEqual(clip.capture_frame_rate,
+                     StrictlyPositiveRational(value.numerator, value.denominator))
 
   def test_shutter_angle(self):
     clip = Clip()
