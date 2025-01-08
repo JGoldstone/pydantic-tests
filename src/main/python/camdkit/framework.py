e09656539b922b92b5b7b6f8652d298733a7fdf2
Joseph Goldstone <joseph.goldstone@mac.com>
2025-01-07 21:24:27 -0500

    Adjust import forwarding contents of model.py and framework.py

--- src/main/python/camdkit/framework.py
+++ src/main/python/camdkit/framework.py
@@ -1,11 +1,14 @@
-from camdkit.versioning_types import (OPENTRACKIO_PROTOCOL_NAME,
-                                      OPENTRACKIO_PROTOCOL_VERSION,
-                                      VersionedProtocol)
-from camdkit.tracker_types import GlobalPosition
-# from camdkit.timing_types import TimingMode as TimingModeEnum
+
+
+from fractions import Fraction
+
+from camdkit.lens_types import (FizEncoders, RawFizEncoders, ExposureFalloff,
+                                Distortion, DistortionOffset, ProjectionOffset)
 from camdkit.timing_types import SynchronizationSource as SynchronizationSourceEnum
 from camdkit.timing_types import (Timestamp, TimecodeFormat, TimingMode, Timecode,
                                   SynchronizationOffsets, SynchronizationPTP, Synchronization)
+from camdkit.tracker_types import GlobalPosition
 from camdkit.transform_types import Vector3, Rotator3, Transform
-from camdkit.lens_types import (FizEncoders, RawFizEncoders, ExposureFalloff,
-                                Distortion, DistortionOffset, ProjectionOffset)
+from camdkit.versioning_types import (OPENTRACKIO_PROTOCOL_NAME,
+                                      OPENTRACKIO_PROTOCOL_VERSION,
+                                      VersionedProtocol)
--- src/main/python/camdkit/model.py
+++ src/main/python/camdkit/model.py
@@ -1,14 +1,16 @@
 from camdkit.clip import Clip
 
-# In the classic implementation, there were paramaters, and backing types for them. Not here.
-
-from camdkit.timing_types import TimingMode as TimingModeEnum
-from camdkit.lens_types import FizEncoders as LensEncoders
-from camdkit.lens_types import RawFizEncoders as LensRawEncoders
-from camdkit.lens_types import ExposureFalloff as LensExposureFalloff
+from camdkit.camera_types import PhysicalDimensions as Dimensions
 from camdkit.lens_types import Distortion as LensDistortions
 from camdkit.lens_types import DistortionOffset as LensDistortionOffset
+from camdkit.lens_types import ExposureFalloff as LensExposureFalloff
+from camdkit.lens_types import FizEncoders as LensEncoders
 from camdkit.lens_types import ProjectionOffset as LensProjectionOffset
-from camdkit.timing_types import Timestamp as TimingTimestamp
+from camdkit.lens_types import RawFizEncoders as LensRawEncoders
+from camdkit.numeric_types import StrictlyPositiveRational
+from camdkit.timing_types import Synchronization
 from camdkit.timing_types import Timecode as TimingTimecode
+from camdkit.timing_types import Timestamp as TimingTimestamp
+from camdkit.timing_types import TimingMode as TimingModeEnum
 from camdkit.transform_types import Transform as Transforms
+from camdkit.versioning_types import OPENTRACKIO_PROTOCOL_NAME, OPENTRACKIO_PROTOCOL_VERSION
