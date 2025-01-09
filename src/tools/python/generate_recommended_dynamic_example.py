8bd92ba7891fff1f2da52196c7afb8943251413b
Joseph Goldstone <joseph.goldstone@mac.com>
2025-01-09 17:26:37 -0500

    Add ability to supply an argument to example generators to redirect output to a file

---
+++ src/tools/python/generate_recommended_dynamic_example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the SMTPE RIS OSVP Metadata Project
+
+'''CLI tool to generate and validate JSON for an example OpenTrackIO recommended dynamic metadata sample'''
+
+import json
+import sys
+
+from camdkit.examples import get_recommended_dynamic_example
+
+if __name__ == "__main__":
+  json_as_text = json.dumps(get_recommended_dynamic_example(), indent=2)
+  if len(sys.argv) == 2:
+    with open(sys.argv[1], "w") as fp:
+      print(json_as_text, file=fp)
+  else:
+    print(json_as_text)
