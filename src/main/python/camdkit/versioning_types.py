2a1ff1849d19c645d926b42821d53b10331667a7
Joseph Goldstone <joseph.goldstone@mac.com>
2025-01-09 19:30:33 -0500

    Rework protocol versioning so that generated schema matches that of classic camdkit.

--- src/main/python/camdkit/versioning_types.py
+++ src/main/python/camdkit/versioning_types.py
@@ -6,19 +6,24 @@
 
 """Types for versioning protocols"""
 
+from typing import Annotated
+
 from camdkit.compatibility import CompatibleBaseModel
 from camdkit.numeric_types import SingleDigitInt
 from camdkit.string_types import NonBlankUTF8String
+
+from pydantic import Field
 
 __all__ = ['OPENTRACKIO_PROTOCOL_NAME', 'OPENTRACKIO_PROTOCOL_VERSION', 'VersionedProtocol']
 
 OPENTRACKIO_PROTOCOL_NAME = "OpenTrackIO"
 OPENTRACKIO_PROTOCOL_VERSION = (0, 9, 2)
 
+type VersionComponent = Annotated[int, Field(ge=0, le=9)]
 
 class VersionedProtocol(CompatibleBaseModel):
     name: NonBlankUTF8String
-    version: tuple[int, int, int]
+    version: Annotated[tuple[VersionComponent, ...], Field(min_length=3, max_length=3)]
 
     def __init__(self, name: NonBlankUTF8String, version: tuple[SingleDigitInt, SingleDigitInt, SingleDigitInt]):
         super(VersionedProtocol, self).__init__(name=name, version=version)
