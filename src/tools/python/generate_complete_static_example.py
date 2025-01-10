9379d52038fb77f9123b970b7b4877704e2af76d
Joseph Goldstone <joseph.goldstone@mac.com>
2025-01-09 19:29:32 -0500

    Sort keys when outputting JSON for easier comparison with other implementations (e.g. classic).

--- src/tools/python/generate_complete_static_example.py
+++ src/tools/python/generate_complete_static_example.py
@@ -12,7 +12,9 @@ import sys
 from camdkit.examples import get_complete_static_example
 
 if __name__ == "__main__":
-  json_as_text = json.dumps(get_complete_static_example(), indent=2)
+  json_as_text = json.dumps(get_complete_static_example(),
+                            sort_keys=True,
+                            indent=2)
   if len(sys.argv) == 2:
     with open(sys.argv[1], "w") as fp:
       print(json_as_text, file=fp)
--- src/tools/python/generate_recommended_dynamic_example.py
+++ src/tools/python/generate_recommended_dynamic_example.py
@@ -12,7 +12,9 @@ import sys
 from camdkit.examples import get_recommended_dynamic_example
 
 if __name__ == "__main__":
-  json_as_text = json.dumps(get_recommended_dynamic_example(), indent=2)
+  json_as_text = json.dumps(get_recommended_dynamic_example(),
+                            sort_keys=True,
+                            indent=2)
   if len(sys.argv) == 2:
     with open(sys.argv[1], "w") as fp:
       print(json_as_text, file=fp)
