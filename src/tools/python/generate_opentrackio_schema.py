ef9fa050c626b493a85aa25a53b246255084dbe1
Joseph Goldstone <joseph.goldstone@mac.com>
2025-01-23 16:04:09 +1300

    Remove provision for generate_opentrackio_schema.py to take CLI argument indicating a file into which stdout would be redirected.

--- src/tools/python/generate_opentrackio_schema.py
+++ src/tools/python/generate_opentrackio_schema.py
@@ -3,11 +3,5 @@ import json
 import camdkit.model
 
 if __name__ == "__main__":
-  json_as_text = json.dumps(camdkit.model.Clip.make_json_schema(),
-                            sort_keys=True,
-                            indent=2)
-  if len(sys.argv) == 2:
-    with open(sys.argv[1], "w") as fp:
-      print(json_as_text, file=fp)
-  else:
-    print(json_as_text)
+  schema = camdkit.model.Clip.make_json_schema()
+  json.dump(schema, sys.stdout, indent=2)
